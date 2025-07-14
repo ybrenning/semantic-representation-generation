94c3c07bc4c6952ba242eee8a8dd0b72def5ff4b
ybrenning <yannickbrenning2@gmail.com>
2025-07-14 13:12:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+with open("main-grammar.irtg", "r") as f:
+    rules_dict = {}
+    lines = f.readlines()
+    for line in lines:
+        if "->" in line:
+            rules = line.strip().split("->")
+            lhs, rhs = rules[0], rules[1]
+            rhs = rhs.split("}")[-1]
+            _, midright = rhs.split("(")
+            rhs, _ = midright.split(")")
+
+            lhs = lhs.strip()
+            lhs = lhs.replace("!", "") if lhs == "S!" else lhs
+            rhs = rhs.strip().split(",")
+            rhs = [r.strip() for r in rhs]
+            if lhs not in rules_dict:
+                rules_dict[lhs] = [rhs]
+            else:
+                rules_dict[lhs].append(rhs)
+
+    output = ""
+    for lhs, rhs in rules_dict.items():
+        output += lhs + " : "
+        rhs = [" ".join(r) for r in rhs]
+        output += " | ".join(rhs) + "\n\n"
+    print(output)
