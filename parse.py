5c40553aeb7d042232ae3e4d167eca312b8d32fb
ybrenning <yannickbrenning2@gmail.com>
2025-08-26 12:45:03 +0200

    Fix prompt parse

--- parse.py
+++ parse.py
@@ -5,25 +5,25 @@ grammar_path = "grammars/preprocessed-main.irtg"
 in_path = "data/english/in_distribution-english.txt"
 in_path = "data/english/prompt-english.txt"
 
-with open(in_path, "r+") as f:
-    lines = f.readlines()
-    if not lines[0].startswith("//"):
-
-        lines = [
-            " ".join([s.split()[0].lower()] + s.split()[1:-1])
-            for s in lines if s != "\n"
-        ]
-
-        header = """
-        // IRTG unannotated corpus file, v1.0
-        //
-        // interpretation english: de.up.ling.irtg.algebra.StringAlgebra
-
-
-        """
-
-        content = header + "\n".join(lines) + "\n"
-        f.write(content)
+# with open(in_path, "r+") as f:
+#     lines = f.readlines()
+#     if not lines[0].startswith("//"):
+#
+#         lines = [
+#             " ".join([s.split()[0].lower()] + s.split()[1:-1])
+#             for s in lines if s != "\n"
+#         ]
+#
+#         header = """
+#         // IRTG unannotated corpus file, v1.0
+#         //
+#         // interpretation english: de.up.ling.irtg.algebra.StringAlgebra
+#
+#
+#         """
+#
+#         content = header + "\n".join(lines) + "\n"
+#         f.write(content)
 
 out_path = f"data/varfree_lf/{grammar_prefix}-varfree.txt"
 out_path = f"data/varfree_lf/prompt-varfree.txt"
