bf28859334d11688298fe76b49e8618028194b40
ybrenning <yannickbrenning2@gmail.com>
2025-09-08 16:05:53 +0200

    Add lex check before parse

--- parse.py
+++ parse.py
@@ -1,7 +1,8 @@
 import subprocess
 
 grammar_prefix = "in_distribution"
-grammar_path = "grammars/preprocessed-combined.irtg"
+grammar_path_irtg = "grammars/preprocessed-RC_modifying_subject_NP_gen-grammar.irtg"
+grammar_path_ebnf = "grammars/preprocessed-RC_modifying_subject_NP_gen-grammar.ebnf"
 in_path = "data/english/in_distribution-english.txt"
 in_path = "data/english/prompt-english.txt"
 
@@ -28,10 +29,21 @@ in_path = "data/english/prompt-english.txt"
 out_path = f"data/varfree_lf/{grammar_prefix}-varfree.txt"
 out_path = f"data/varfree_lf/prompt-varfree.txt"
 
+from utils import read_grammar
+
+lex = read_grammar(grammar_path_ebnf, lex_only=True)
+with open(in_path, "r") as f:
+    for line in f.readlines():
+        if not line.startswith("//"):
+            for word in line.split(" "):
+                if word != "\n" and word.strip() not in lex:
+                    print(f"Word {word} not in lexicon")
+
+
 command = (
     "java -cp ../alto/build/libs/alto-2.3.8-SNAPSHOT-all.jar "
     "de.up.ling.irtg.script.ParsingEvaluator "
-    f"-g {grammar_path} "
+    f"-g {grammar_path_irtg} "
     "-I english -O semantics=cogs "
     f"-o {out_path} "
     "--no-derivations "
