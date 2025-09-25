4780a8d59c08274679ac36c540f8c37e522f38d1
ybrenning <yannickbrenning2@gmail.com>
2025-09-25 12:30:06 +0200

    Change to argparse

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-import sys
+import argparse
 
 from generation.src.utils import test_pipeline
 from parse import format_sents, parse_sents
 from evaluate import evaluate_parse
 from prompts.prompt import prompt_from_grammar
-from preprocess import handle_null_sents
+from preprocess import (
+    handle_null_sents,
+    handle_incorrect_sents,
+    handle_repetition_sents
+)
 from utils import get_safe_filename
 
 
-def generation_loop(grammar_path, n_prompts):
+def generation_loop(grammar_path, n_prompts, n_sets):
     responses = ""
     for _ in range(n_prompts):
         # Maybe also save the generated prompts?
-        prompt = prompt_from_grammar(grammar_path, n_sets=3, k=30)
+        prompt = prompt_from_grammar(
+            grammar_path,
+            n_sets=n_sets,
+            k=30
+        )
         print(prompt)
 
-        response = test_pipeline(prompt, temperature=0.4, top_p=0.9)
+        response = test_pipeline(
+            prompt,
+            temperature=0.4,
+            top_p=0.9
+        )
         responses += response + "\n"
 
     response_path = "prompts/prompt-newest"
@@ -36,14 +48,42 @@ def generation_loop(grammar_path, n_prompts):
     return response_path
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Execute data generation pipeline"
+    )
+    parser.add_argument(
+        "grammar_path",
+        type=str,
+        help="Path to the IRTG grammar file (.irtg)."
+    )
+    parser.add_argument(
+        "n_prompts",
+        type=int,
+        help="Number of times to prompt."
+    )
+    parser.add_argument(
+        "n_sets",
+        type=int,
+        help="Number of sentence sets per prompt"
+    )
+
+    args = parser.parse_args()
+
+    if not args.grammar_path.endswith(".irtg"):
+        parser.error("The grammar file must have a .irtg extension")
+
+    return args
+
+
 def main():
-    grammar_path = sys.argv[1]
-    n_prompts = int(sys.argv[2])
-    assert grammar_path.endswith(".irtg"), "Provide IRTG grammar"
-    assert isinstance(n_prompts, int), "Provide no. of times to prompt"
+    args = parse_args()
+    grammar_path = args.grammar_path
+    n_prompts = args.n_prompts
+    n_sets = args.n_sets
 
     # Generation step
-    response_path = generation_loop(grammar_path, n_prompts)
+    response_path = generation_loop(grammar_path, n_prompts, n_sets)
 
     # Format and parse steps
     sent_path = format_sents(response_path)
@@ -51,10 +91,8 @@ def main():
 
     # Filtering step
     en_lines, vf_lines = handle_null_sents(sent_path, varfree_path)
-    print(len(vf_lines))
-    assert 0
-
-    # Postprocessing step
+    en_lines, vf_lines = handle_incorrect_sents(en_lines, vf_lines)
+    en_lines, vf_lines = handle_repetition_sents(en_lines, vf_lines)
 
     # Evaluation step
     evaluate_parse(
@@ -63,6 +101,9 @@ def main():
         show_stats=True,
         show_oov=True
     )
+    assert 0
+
+    # Postprocessing step
 
 
 if __name__ == "__main__":
