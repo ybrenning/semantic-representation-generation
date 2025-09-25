41d94533c48ed7ae1277be6ff97d33e18c441b56
ybrenning <yannickbrenning2@gmail.com>
2025-09-25 14:52:31 +0200

    Repeat generation until

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import argparse
 from generation.src.utils import test_pipeline
 from parse import format_sents, parse_sents
 from evaluate import evaluate_parse
-from prompts.prompt import prompt_from_grammar
-from preprocess import (
+from generation.prompt import prompt_from_grammar
+from postprocess import (
     handle_null_sents,
     handle_incorrect_sents,
     handle_repetition_sents
 )
-from utils import get_safe_filename
+from utils import get_safe_filename, en_header
 
 
-def generation_loop(grammar_path, n_prompts, n_sets):
+def generation_loop(grammar_path, n_prompts, n_sets, verbose=False):
     responses = ""
     for _ in range(n_prompts):
         # Maybe also save the generated prompts?
@@ -21,16 +21,16 @@ def generation_loop(grammar_path, n_prompts, n_sets):
             n_sets=n_sets,
             k=30
         )
-        print(prompt)
 
         response = test_pipeline(
             prompt,
             temperature=0.4,
-            top_p=0.9
+            top_p=0.9,
+            verbose=verbose
         )
         responses += response + "\n"
 
-    response_path = "prompts/prompt-newest"
+    response_path = "generation/responses/prompt-newest"
 
     suffix = (
         f"-{n_prompts}-responses.txt" if n_prompts > 1 else "-response.txt"
@@ -67,6 +67,11 @@ def parse_args():
         type=int,
         help="Number of sentence sets per prompt"
     )
+    parser.add_argument(
+        "-v", "--verbose",
+        action="store_true",
+        help="Enable verbose output."
+    )
 
     args = parser.parse_args()
 
@@ -81,29 +86,56 @@ def main():
     grammar_path = args.grammar_path
     n_prompts = args.n_prompts
     n_sets = args.n_sets
+    verbose = args.verbose
 
-    # Generation step
-    response_path = generation_loop(grammar_path, n_prompts, n_sets)
+    n_sents = n_prompts * n_sets * 6
+    english, semantics = [], []
+    while len(semantics) != n_sents:
+        # Generation step
+        response_path = generation_loop(
+            grammar_path,
+            n_prompts,
+            n_sets,
+            verbose
+        )
 
-    # Format and parse steps
-    sent_path = format_sents(response_path)
-    varfree_path = parse_sents(sent_path, grammar_path)
+        # Format and parse steps
+        sent_path = format_sents(response_path)
+        varfree_path = parse_sents(sent_path, grammar_path)
 
-    # Filtering step
-    en_lines, vf_lines = handle_null_sents(sent_path, varfree_path)
-    en_lines, vf_lines = handle_incorrect_sents(en_lines, vf_lines)
-    en_lines, vf_lines = handle_repetition_sents(en_lines, vf_lines)
+        # Filtering step
+        en_lines, vf_lines = handle_null_sents(sent_path, varfree_path)
+        en_lines, vf_lines = handle_incorrect_sents(en_lines, vf_lines)
+        en_lines, vf_lines = handle_repetition_sents(en_lines, vf_lines)
 
-    # Evaluation step
-    evaluate_parse(
-        varfree_path,
-        grammar_path,
-        show_stats=True,
-        show_oov=True
-    )
-    assert 0
+        # Evaluation step
+        evaluate_parse(
+            varfree_path,
+            grammar_path,
+            show_stats=True,
+            show_oov=verbose
+        )
+
+        if not english and not semantics:
+            english = en_lines
+            semantics = vf_lines
+        else:
+            remainder = n_sents - len(semantics)
+            assert remainder > 0
+            english.extend(en_lines[:remainder])
+            semantics.extend(vf_lines[:remainder])
+
+    sent_path_out = "data/english/" + sent_path.split("/")[-1]
+    varfree_path_out = "data/varfree_lf/" + sent_path.split("/")[-1]
+
+    with open(sent_path_out, "w") as f:
+        f.write(en_header + "".join(en_lines))
+
+    with open(varfree_path_out, "w") as f:
+        f.write("".join(vf_lines))
 
     # Postprocessing step
+    ...
 
 
 if __name__ == "__main__":
