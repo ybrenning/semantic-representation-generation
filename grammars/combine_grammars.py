fe33c9671c809c6545fbdc36230f9ac5dad8e3d9
ybrenning <yannickbrenning2@gmail.com>
2025-10-13 14:18:34 +0200

    Create minimal base grammar

--- grammars/combine_grammars.py
+++ grammars/combine_grammars.py
@@ -15,15 +15,44 @@ def clean_line(line):
 
 
 grammars = [
-    "base/main-grammar.irtg",
-    "base/RC_modifying_dobj_NP_main-grammar.irtg",
-    "base/RC_modifying_subject_NP_gen-grammar.irtg"
+    # "base/main-grammar.irtg",
+    # "base/RC_modifying_dobj_NP_main-grammar.irtg",
+    # "base/RC_modifying_subject_NP_gen-grammar.irtg"
+    "g1.irtg",
+    "g2.irtg",
+    "g3.irtg",
+    "g4.irtg",
+    "g5.irtg",
+    "g6.irtg"
 ]
 
-out_file = "combined-grammars.irtg"
+out_file = "combined-grammars-6.irtg"
 
+header = """
+
+
+interpretation english: de.up.ling.irtg.algebra.StringAlgebra
+interpretation semantics: de.saar.coli.algebra.OrderedFeatureTreeAlgebra
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 seen = set()
 with open(out_file, "w") as f_out:
+    f_out.write(header)
     for grammar in grammars:
         with open(grammar, "r") as f_in:
             skip_count = 0
@@ -43,6 +72,8 @@ with open(out_file, "w") as f_out:
                         print("skipping", line)
                         print(cleaned)
                         skip_count = 2
+                elif line.startswith("interpretation"):
+                    continue
                 else:
                     print("writing", line)
                     f_out.write(line)
