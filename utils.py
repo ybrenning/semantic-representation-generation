86d6805d9daefdfba7f1d8b4a622328ef91756c7
ybrenning <yannickbrenning2@gmail.com>
2025-10-31 15:53:00 +0100

    Save rec grammars

--- utils.py
+++ utils.py
@@ -97,12 +97,80 @@ def get_safe_filename(filename):
 
 
 def generate_rec_pp_rules(n):
+    grammar_path = f"grammars/slog-rec_pp_{n}.irtg"
+    base_grammar = """
+{## Sentences and verb phrases ##}
+S! -> r{{ cnt.next() }}(NP_animate_nsubj, VP_external) [0.9]
+[english] *(?1, ?2)
+[semantics] pre_agent(?2, ?1)
+
+VP_external -> r{{ cnt.next() }}(V_unacc, NP_dobj) [0.2]
+[english] *(?1, ?2)
+[semantics] theme(?1, ?2)
+
+VP_external -> r{{ cnt.next() }}(V_trans_omissible, NP_dobj) [0.2]
+[english] *(?1, ?2)
+[semantics] theme(?1, ?2)
+
+VP_external -> r{{ cnt.next() }}(V_trans_not_omissible, NP_dobj) [0.2] 
+[english] *(?1, ?2)
+[semantics] theme(?1, ?2)
+
+VP_external -> I_dobj_before_iobj_F{{ cnt.next() }}(V_dat, NP_inanimate_dobj, PP_iobj) [0.2]
+[english] *(*(?1, ?2), ?3)
+[semantics] recipient(theme(?1, ?2), ?3)
+
+VP_external -> I_dobj_after_iobj_F{{ cnt.next() }}(V_dat, NP_animate_iobj, NP_inanimate_dobj) [0.2]
+[english] *(*(?1, ?2), ?3)
+[semantics] theme(recipient(?1, ?2), ?3)
+
+
+{## Noun phrases ##}
+
+NP_dobj -> r{{ cnt.next() }}(NP_inanimate_dobj) [0.5]
+[english] ?1
+[semantics] ?1
+
+NP_dobj -> r{{ cnt.next() }}(NP_animate_dobj) [0.5]
+[english] ?1
+[semantics] ?1
+
+NP_animate_dobj -> S_PP_E{{ cnt.next() }}(Det, N_common_animate_dobj, PP_loc_1) [1]
+[english] *(*(?1, ?2), ?3)
+[semantics] nmod(pre_det(?2, ?1), ?3)
+
+NP_animate_iobj -> r{{ cnt.next() }}(Det, N_common_animate_iobj) [0.5]
+[english] *(?1, ?2)
+[semantics] pre_det(?2, ?1)
+
+NP_animate_iobj -> r{{ cnt.next() }}(N_prop_iobj) [0.5]
+[english] ?1
+[semantics] ?1
+
+
+
+NP_animate_nsubj -> r{{ cnt.next() }}(Det, N_common_animate_nsubj) [0.5]
+[english] *(?1, ?2)
+[semantics] pre_det(?2, ?1)
+
+NP_animate_nsubj -> r{{ cnt.next() }}(N_prop_nsubj) [0.5]
+[english] ?1
+[semantics] ?1
+
+NP_inanimate_dobj -> S_PP_E{{ cnt.next() }}(Det, N_common_inanimate_dobj, PP_loc_1) [1]
+[english] *(*(?1, ?2), ?3)
+[semantics] nmod(pre_det(?2, ?1), ?3)
+
+PP_iobj -> r{{ cnt.next() }} (P_iobj,NP_animate_iobj)[1]
+[english] *(?1, ?2)
+[semantics] pre_case(?2, ?1)
+    """
     out = []
     for prep in ["on", "in", "beside"]:
         for i in reversed(range(n)):
             out.append(
                 f"PP_loc_{i} -> "
-                "r{{{{ cnt.next() }}}} (P_{prep},NP_{prep}_{i}) [0.0]"
+                f"r{{{{ cnt.next() }}}} (P_{prep},NP_{prep}_{i}) [0.0]"
             )
             out.append("[english] *(?1, ?2)")
             out.append("[semantics] pre_case(?2, ?1)")
@@ -116,10 +184,13 @@ def generate_rec_pp_rules(n):
                 out.append("[semantics] pre_det(?2, ?1)")
             else:
                 out.append(
-                    f"NP_{prep}_{i} "
-                    "-> S_PP_E{{{{ cnt.next() }}}} (Det, N_{prep}, PP_loc_{i-1}) [0.0]"
+                    f"NP_{prep}_{i} -> "
+                    f"S_PP_E{{{{ cnt.next() }}}} "
+                    f"(Det, N_{prep}, PP_loc_{i-1}) [0.0]"
                 )
                 out.append("[english] *(*(?1, ?2), ?3)")
                 out.append("[semantics] nmod(pre_det(?2, ?1), ?3)")
             out.append("")
-    print("\n".join(out))
+
+    with open(grammar_path, "w") as f:
+        f.write(base_grammar + "\n".join(out))
