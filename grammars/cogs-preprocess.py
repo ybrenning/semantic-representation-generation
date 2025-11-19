2a92afa8bf76da01e2912c965c9dd2a259f479b0
ybrenning <yannickbrenning2@gmail.com>
2025-11-19 14:12:48 +0100

    Overhaul preprocess code

--- grammars/cogs-preprocess.py
+++ grammars/cogs-preprocess.py
@@ -1,7 +1,7 @@
-from jinja2 import *
+import argparse
 import numpy as np
 from scipy.stats import zipf
-import sys
+from jinja2 import Environment, FileSystemLoader
 
 
 def align_rules_with_tabs(input_text):
@@ -19,52 +19,73 @@ def align_rules_with_tabs(input_text):
         else:
             updated_lines.append(line)
 
-    return '\n'.join(updated_lines)
+    return "\n".join(updated_lines)
 
-## Counter object for numbering the rules
+
 class _Counter(object):
-  def __init__(self, start_value=1):
-    self.value=start_value
+    def __init__(self, start_value=1):
+        self.value = start_value
 
-  def current(self):
-    return self.value
+    def current(self):
+        return self.value
 
-  def next(self):
-    v=self.value
-    self.value+=1
-    return v
-    
+    def next(self):
+        v = self.value
+        self.value += 1
+        return v
 
-## Assign Zipfian distribution to vocab
+
+# Assign Zipfian distribution to vocab
 def normalize(probs):
-    ### AK: Why is this not simply probs/sum(probs)?
-    ### The code below does not maintain the ratios between the different probabilities.
+    # AK: Why is this not simply probs/sum(probs)?
+    # The code below does not maintain the ratios between
+    # the different probabilities.
     leftover_prob = 1-sum(probs)
     probs = probs + leftover_prob/len(probs)
     return probs
 
+
 def generate_vocab_probabilities(words):
     a = 1.4
-    k = np.array(list(range(1,101)))
-    probs = zipf.pmf(np.array(range(1,len(words)+1)), a)
+    probs = zipf.pmf(np.array(range(1, len(words)+1)), a)
     probs = normalize(probs)
     return zip(words, probs)
 
 
+def main():
+    parser = argparse.ArgumentParser(
+        description="Execute data generation pipeline"
+    )
+    parser.add_argument(
+        "grammar_path",
+        type=str,
+        help="Path to the IRTG grammar file"
+    )
+    parser.add_argument(
+        "-v", "--verbose",
+        action="store_true",
+        help="Enable verbose output"
+    )
 
+    args = parser.parse_args()
+    grammar_path = args.grammar_path
+    verbose = args.verbose
 
+    # Process the templates
+    env = Environment(loader=FileSystemLoader("."))
+    env.globals["counter"] = _Counter
+    env.filters["zipf"] = generate_vocab_probabilities
 
+    template = env.get_template("specify_grammar.irtg")
+    temp_str = template.render(
+        grammar_path=grammar_path
+    )
 
-## Process the templates
-env = Environment(loader=FileSystemLoader("."))
-env.globals['counter'] = _Counter
-env.filters['zipf'] = generate_vocab_probabilities
+    aligned_string = align_rules_with_tabs(temp_str)
+    if verbose:
+        print(aligned_string)
 
-template = env.get_template(sys.argv[1])
-temp_str = template.render()
-
-# print(type(template.render()))
-aligned_string = align_rules_with_tabs(temp_str)
-print(aligned_string)
-# print(template.render())
-
+    grammar_path = "preprocessed-" + grammar_path
+    with open(grammar_path, "w") as f:
+        f.write(aligned_string)
+        print("Saved preprocessed grammar to", grammar_path)
